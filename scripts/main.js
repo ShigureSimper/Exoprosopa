6df1db44aa492ba82e2fe370a88805352427087c
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-03-27 08:58:20 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const exoprosopa = Vars.mods.locateMod("exoprosopa").meta;
+
+exoprosopa.displayName = Core.bundle.get("mod.exoprosopa.name");
+exoprosopa.author = Core.bundle.get("mod.exoprosopa.author");
+exoprosopa.description = Core.bundle.get("mod.exoprosopa.description");
+exoprosopa.subtitle = Core.bundle.get("mod.exoprosopa.subtitle");
